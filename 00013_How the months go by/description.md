d8b674a394ac57d9ee5cc8883e0dfb6609a609b7
nadia@mumuki.org <nadia@mumuki.org>
2023-03-28 20:39:48 +0000

    Mumuki Export on 2023-03-28 20:39:48 +0000

---
+++ 00013_How the months go by/description.md
@@ -0,0 +1,14 @@
+Now that we know how to do mappings, let's practice! :muscle:
+
+> Define the function `months` that, given a list of dictionaries, returns a list of months. :calendar:
+>
+```python
+ム months([
+	{ "month": "January", "profit": 870 },
+	{ "month": "February", "profit": 1000 },
+	{ "month": "March", "profit": 1020 },
+	{ "month": "April", "profit": 2300 },
+	{ "month": "May", "profit": -10 }
+  ])
+["January", "February", "March", "April", "May"]
+```
--- 00013_¡Cómo pasan los meses!/test.py
+++
@@ -1,6 +0,0 @@
-
-  def test_meses_de_un_trimestre_devuelve_tres_meses(self):
-    self.assertEqual(meses([{ "mes": "enero", "ganancia": 10 }, { "mes": "febrero", "ganancia": 2 }, { "mes": "marzo", "ganancia": 5 }]), ["enero", "febrero", "marzo"])
-  
-  def test_meses_de_un_semestre_devuelve_seis_meses(self):
-    self.assertEqual(meses([{ "mes": "enero", "ganancia": 10 }, { "mes": "febrero", "ganancia": 2 }, { "mes": "marzo", "ganancia": 5 }, { "mes": "abril", "ganancia": 8 }, { "mes": "mayo", "ganancia": 12 }, { "mes": "junio", "ganancia": 25 }]), ["enero", "febrero", "marzo", "abril", "mayo", "junio"])
